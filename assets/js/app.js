8b8e3c7a4cfffa5a27412b6b14a4ff00eb0f6e4f
WhizSid <whizsid@aol.com>
2019-06-29 23:05:18 +0530

    Fixed issues in responsive look

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,13 @@
+var menuOpen = false;
+
 $(document).ready(function() {
     $(".sec-5-slider").slick({
         // normal options...
         infinite: true,
         mobileFirst: true,
         // the magic
-        responsive: [{
+        responsive: [
+            {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
@@ -27,7 +30,7 @@ $(document).ready(function() {
         arrows: true,
         dots: false,
 
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1
     });
 
@@ -38,5 +41,15 @@ $(document).ready(function() {
         $(this).addClass('active')
         $('.sec-6-desc').removeClass('active');
         $('.sec-6-desc:nth-child(' + show + ')').addClass('active');
+    })
+
+    $('.navbar-toggler').click(function(){
+        if(menuOpen){
+            $('.navbar-expand').addClass('menu-expanded');
+            menuOpen = false;
+        } else {
+            $('.navbar-expand').removeClass('menu-expanded');
+            menuOpen = true;
+        }
     })
 });
